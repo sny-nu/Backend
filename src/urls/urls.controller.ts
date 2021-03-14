b30b1606d73369484f73c9d3ddfac4a04531db6a
Thomas Lamars <thomas@lamars.nl>
2021-03-14 18:34:04 +0100

    Made small change

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Url } from 'src/entities/url.entity';
 import { UrlsService } from './urls.service';
 
@@ -19,6 +19,23 @@ export class UrlsController
     @Post("v1/url")
     async create(@Body() url: Url): Promise<Url>
     {
+        if(await this.checkUrl(url.originalUrl) == null)
+        {
+            throw new BadRequestException(`The URL that was provided, '${url.originalUrl}' is not a valid URL`)
+        }
+
+        if (!url.originalUrl.includes("https://") && !url.originalUrl.includes("http://")) {
+                const completeUrl = "http://" + url;
+                url.originalUrl = completeUrl;
+        }
+
         return this.urlsService.create(url);
+    }
+
+
+    async checkUrl(url: string): Promise<RegExpMatchArray>
+    {
+        const regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+        return url.match(regex);
     }
 }
