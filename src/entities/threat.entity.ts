e7ee2e676f6725b1302bc93b30ac99dd7640fc3c
Thomas Lamars <thomas@lamars.nl>
2021-03-21 18:43:03 +0100

    Updated threat entity

--- src/entities/threat.entity.ts
+++ src/entities/threat.entity.ts
@@ -3,13 +3,12 @@ import { Url } from "./url.entity";
 
 @Entity()
 @Unique("url_threat", ["urlHash", "type"])
-@Unique("urlHash", ["urlHash"])
 export class Threat
 {
     @PrimaryColumn({ length: 50 })
     urlHash: string;
 
-    @Column({ length: 50 })
+    @PrimaryColumn({ length: 50 })
     type: string;
 
     @ManyToOne(type => Url, url => url.threats)
