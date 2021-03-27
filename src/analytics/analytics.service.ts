105db253c3e7b886d0c5efd241ed304750c91a0c
Thomas Lamars <thomas@lamars.nl>
2021-03-27 17:05:11 +0100

    Changed small things

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -20,21 +20,19 @@ export class AnalyticsService
 
     async addAnalytics(ua: any, urlHash: string) 
     {
-
-        console.log(Bowser);
         const bowser = Bowser.getParser(ua);
         const uaParser = parser(ua);
 
-        const analytics = new Analytics();
-        analytics.browser = bowser.getBrowserName();
-        analytics.browserVersion = bowser.getBrowserVersion();
-        analytics.engine = bowser.getEngineName();
-        analytics.os = bowser.getOSName();
-        analytics.osVersion = bowser.getOSVersion();
-        analytics.device = uaParser.device.model == undefined ? "Unknown" : uaParser.device.model;
-        analytics.deviceType = bowser.getPlatformType(); 
-        analytics.deviceVendor = bowser.getPlatform().vendor != undefined ? bowser.getPlatform().vendor : (uaParser.device.vendor == undefined ? "Unknown" : uaParser.device.vendor);
-        analytics.urlHash = urlHash;
+        const analytics          = new Analytics();
+        analytics.browser        = bowser.getBrowserName()     == '' ? "Unknown" : bowser.getBrowserName();
+        analytics.browserVersion = bowser.getBrowserVersion()  == '' ? "Unknown" : bowser.getBrowserVersion();
+        analytics.engine         = bowser.getEngineName()      == '' ? "Unknown" : bowser.getEngineName();
+        analytics.os             = bowser.getOSName()          == '' ? "Unknown" : bowser.getOSName();
+        analytics.osVersion      = bowser.getOSVersion()       == '' ? "Unknown" : bowser.getOSVersion();
+        analytics.device         = uaParser.device.model       == undefined ? "Unknown" : uaParser.device.model;
+        analytics.deviceType     = bowser.getPlatformType()    == '' ? "Unknown" : bowser.getPlatformType(); 
+        analytics.deviceVendor   = bowser.getPlatform().vendor != undefined ? bowser.getPlatform().vendor : (uaParser.device.vendor == undefined ? "Unknown" : uaParser.device.vendor);
+        analytics.urlHash        = urlHash;
 
         await this.analyticsRepository.addAnalyticsToUrl(analytics);
     }
